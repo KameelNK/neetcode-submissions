a2b7d74a1eb56977a149ae5491c655d909091eb8
Kameel Anayi <94212382+KameelNK@users.noreply.github.com>
2026-04-18 16:11:27 -0400

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.java
@@ -0,0 +1,25 @@
+class Solution {
+
+    public boolean hasDuplicate(int[] nums) {
+        
+        HashSet<Integer> checker = new HashSet<>();
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (checker.contains(nums[i])) {
+
+                return true;
+
+            }
+
+            else {
+                checker.add(nums[i]);
+            }
+
+
+        }
+        
+        return false;
+        
+    }
+}
